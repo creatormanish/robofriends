0aaceabcbea2170267a2d1bce3de068b07f53f0e
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2023-03-02 19:48:13 +0530

    Updates

---
+++ precache-manifest.3cf6f05a015a32cdea00b651b96a4d16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "361d7cc1d57e4827bdcd",
+    "url": "/robofriends/static/css/main.f1f356a6.chunk.css"
+  },
+  {
+    "revision": "361d7cc1d57e4827bdcd",
+    "url": "/robofriends/static/js/main.361d7cc1.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "0f0fd00cdab379c11a1d",
+    "url": "/robofriends/static/css/2.57c03eb8.chunk.css"
+  },
+  {
+    "revision": "0f0fd00cdab379c11a1d",
+    "url": "/robofriends/static/js/2.0f0fd00c.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cc53be390ee6f78e1f8c6708378fabd7",
+    "url": "/robofriends/index.html"
+  }
+];
