c9d5e3361e39df4ca43c6059bdb010f11630d3cb
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2023-03-02 19:38:00 +0530

    Initial-Commit

---
+++ src/robots.js
@@ -0,0 +1,100 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Manish',
+    username: 'manish',
+    email: 'Manish@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ashitosh',
+    username: 'ashitosh',
+    email: 'Ashitosh@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Kunal',
+    username: 'kunal',
+    email: 'Kunal@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Sumit',
+    username: 'sumit',
+    email: 'Sumit@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Krishna',
+    username: 'krishna',
+    email: 'Krishna@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Dhananjay',
+    username: 'dhananjay',
+    email: 'Dhananjay@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Ankit',
+    username: 'ankit',
+    email: 'Ankit@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Rinshu',
+    username: 'rinshu',
+    email: 'Rinshu@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Vishwajeet',
+    username: 'vishwajeet',
+    email: 'Vishwajeet@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Ganesh',
+    username: 'ganesh',
+    email: 'ganesh@gmail.com'
+  },
+  {
+    id: 11,
+    name: 'Abhimanyu',
+    username: 'abhimanyu',
+    email: 'Abhimanyu@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'Jash',
+    username: 'jash',
+    email: 'jash@gmail.com'
+  },
+  {
+    id: 13,
+    name: 'Tanmay',
+    username: 'tanmay',
+    email: 'Tanmay@gmail.com'
+  },
+  {
+    id: 14,
+    name: 'Pratik',
+    username: 'pratik',
+    email: 'Pratik@gmail.com'
+  },
+  {
+    id: 15,
+    name: 'Shivam',
+    username: 'shivam',
+    email: 'Shivam@gmail.com'
+  },
+  {
+    id: 16,
+    name: 'Roshan',
+    username: 'roshan',
+    email: 'Roshan@gmail.com'
+  }
+];
+
+export default robots;
